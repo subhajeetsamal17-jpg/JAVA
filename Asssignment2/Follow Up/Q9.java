977ad161125f926ae19559bf37763831f0312730
subhajeetsamal17-jpg <subhajeetsamal17@gmail.com>
2026-09-05 20:48:00 +0530

    Update Q9.java

--- Asssignment2/Follow Up/Q9.java
+++ Asssignment2/Follow Up/Q9.java
@@ -4,24 +4,22 @@ class Q9
 {
     void display(int id, String name, int dept, double salary)
     {
-        System.out.println("\nEmployee Details");
-        System.out.println("-------------------------");
-        System.out.println("Employee ID       : " + id);
-        System.out.println("Employee Name     : " + name);
-        System.out.println("Department Number : " + dept);
-        System.out.println("Salary            : ₹" + salary);
+        System.out.println("Employee ID = " + id);
+        System.out.println("Employee Name = " + name);
+        System.out.println("Department Number = " + dept);
+        System.out.println("Salary = ₹" + salary);
 
         if(salary > 50000)
         {
-            System.out.println("Salary Status     : Above ₹50,000");
+            System.out.println("Salary is above ₹50,000");
         }
         else if(salary < 50000)
         {
-            System.out.println("Salary Status     : Below ₹50,000");
+            System.out.println("Salary is below ₹50,000");
         }
         else
         {
-            System.out.println("Salary Status     : Equal to ₹50,000");
+            System.out.println("Salary is equal to ₹50,000");
         }
     }
 
@@ -29,17 +27,20 @@ class Q9
     {
         if(args.length != 4)
         {
-            System.out.println("Error: Please provide exactly 4 arguments.");
-            System.out.println("Format: Employee_ID Employee_Name Department_Number Salary");
+            System.out.println("Error: Please enter exactly 4 arguments.");
             return;
         }
 
-        int id = Integer.parseInt(args[0]);
-        String name = args[1];
-        int dept = Integer.parseInt(args[2]);
-        double salary = Double.parseDouble(args[3]);
+        Scanner sc1 = new Scanner(args[0]);
+        Scanner sc2 = new Scanner(args[2]);
+        Scanner sc3 = new Scanner(args[3]);
 
-        Q9 obj = new Q9();
+        int id = sc1.nextInt();
+        String name = args[1];
+        int dept = sc2.nextInt();
+        double salary = sc3.nextDouble();
+
+        Q3 obj = new Q3();
 
         obj.display(id, name, dept, salary);
     }
