c86768ef907b51b639ea230b4867b06526f0a686
subhajeetsamal17-jpg <subhajeetsamal17@gmail.com>
2026-09-05 20:49:01 +0530

    Refactor Q10.java to Q4.java with argument parsing

--- Asssignment2/Follow Up/Q10.java
+++ Asssignment2/Follow Up/Q10.java
@@ -1,10 +1,34 @@
 import java.util.*;
 
-class Q10
+class Q4
 {
-    void display(String sic, String name, String branch,
-                 int m1, int m2, int m3, int m4, int m5, int m6)
+    public static void main(String args[])
     {
+        if(args.length != 9)
+        {
+            System.out.println("Error: Please enter exactly 9 arguments.");
+            return;
+        }
+
+        Scanner sc1 = new Scanner(args[0]);
+        Scanner sc2 = new Scanner(args[3]);
+        Scanner sc3 = new Scanner(args[4]);
+        Scanner sc4 = new Scanner(args[5]);
+        Scanner sc5 = new Scanner(args[6]);
+        Scanner sc6 = new Scanner(args[7]);
+        Scanner sc7 = new Scanner(args[8]);
+
+        int sic = sc1.nextInt();
+        String name = args[1];
+        String branch = args[2];
+
+        int m1 = sc2.nextInt();
+        int m2 = sc3.nextInt();
+        int m3 = sc4.nextInt();
+        int m4 = sc5.nextInt();
+        int m5 = sc6.nextInt();
+        int m6 = sc7.nextInt();
+
         int total = m1 + m2 + m3 + m4 + m5 + m6;
         double average = total / 6.0;
 
@@ -35,38 +59,11 @@ class Q10
             grade = 'F';
         }
 
-        System.out.println("\nStudent Details");
-        System.out.println("-------------------------");
-        System.out.println("SIC       : " + sic);
-        System.out.println("Name      : " + name);
-        System.out.println("Branch    : " + branch);
-        System.out.println("Total     : " + total);
-        System.out.println("Average   : " + average);
-        System.out.println("Grade     : " + grade);
-    }
-
-    public static void main(String args[])
-    {
-        if(args.length != 9)
-        {
-            System.out.println("Error: Please provide exactly 9 arguments.");
-            System.out.println("Format: SIC Name Branch Mark1 Mark2 Mark3 Mark4 Mark5 Mark6");
-            return;
-        }
-
-        String sic = args[0];
-        String name = args[1];
-        String branch = args[2];
-
-        int m1 = Integer.parseInt(args[3]);
-        int m2 = Integer.parseInt(args[4]);
-        int m3 = Integer.parseInt(args[5]);
-        int m4 = Integer.parseInt(args[6]);
-        int m5 = Integer.parseInt(args[7]);
-        int m6 = Integer.parseInt(args[8]);
-
-        Q10 obj = new Q10();
-
-        obj.display(sic, name, branch, m1, m2, m3, m4, m5, m6);
+        System.out.println("SIC = " + sic);
+        System.out.println("Name = " + name);
+        System.out.println("Branch = " + branch);
+        System.out.println("Total Marks = " + total);
+        System.out.println("Average Marks = " + average);
+        System.out.println("Grade = " + grade);
     }
 }
